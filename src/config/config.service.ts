a70a06fc75960abe54c4a5a4e53e97a07c3471cb
Petr Molchanov <p.molchanov@profsoft.pro>
2023-03-26 00:36:21 -0300

    https://argentina-turkish-dev.atlassian.net/browse/DPMK-97

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -12,11 +12,13 @@ export class ConfigService {
     const levels_difficulty = enumToObject(CourseLevelDifficultlyEnum);
     const user_roles = enumToObject(UserRoleEnum);
     const test_progress_statuses = enumToObject(ProgressStatusEnum);
+    const bot_name = process.env.TELEGRAM_API_NAME;
     return {
       course_statuses,
       levels_difficulty,
       user_roles,
       test_progress_statuses,
+      bot_name,
     };
   }
 }
