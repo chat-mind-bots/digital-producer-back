2513530ddb00b5439d6286a6c6b81b27fe6a87d9
Petr Molchanov <p.molchanov@rogii.com>
2023-10-02 21:32:05 -0300

    changes

--- src/bot/keyboards/auth.keyboard.ts
+++ src/bot/keyboards/auth.keyboard.ts
@@ -3,8 +3,7 @@ import * as process from 'process';
 
 export const authKeyboard = (href: string) =>
   Markup.inlineKeyboard([
-    Markup.button.url('Войти', href),
     ...(process.env.MODE !== 'LOCAL'
       ? [Markup.button.login('Авторизация на сайте', href)]
-      : []),
+      : [Markup.button.url('Войти', href)]),
   ]);
